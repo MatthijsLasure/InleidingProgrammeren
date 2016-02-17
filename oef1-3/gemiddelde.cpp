7905f6f09dd8a56bbda6577005a9640d1b22d44a
Matthijs Lasure <Matthijs.Lasure@student.uantwerpen.be>
2016-02-17 18:34:59 +0100

    Oefening 3

---
+++ oef1-3/gemiddelde.cpp
@@ -0,0 +1,28 @@
+/*
+ * gemiddelde.cpp
+ *
+ *  Created on: 17 Feb 2016
+ *      Author: Matthijs
+ *
+ * 3. Lees 5 getallen in, en print het gemiddelde van die getallen af.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float rainbow = 0;
+	float rainbowAverage = 0;
+	int FucksGiven = 5; // Aantal getallen
+
+	for(int Fucks = 5; Fucks > 0; Fucks--) {
+		cout << "Voer een getal in (nog " << Fucks << " over): ";
+		cin >> rainbow;
+		rainbowAverage += rainbow;
+	}
+
+	rainbowAverage = rainbowAverage / FucksGiven;
+
+	cout << "Het gemiddelde is " << rainbowAverage;
+}
