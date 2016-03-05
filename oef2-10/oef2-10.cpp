68436a9847bdb297ebbdd235380c8d89ad91c39a
uauser <uauser@trusty-ckw>
2016-03-05 11:22:24 +0100

    Oefening 2-10

--- oef2-10/oef2-10.cpp
+++ oef2-10/oef2-10.cpp
@@ -17,7 +17,19 @@ int main() {
 	cout << "Geef een getal? ";
 	cin >> getal;
 
-
+	// Main loop: 1 regel per loop
+	for (int i = 0; i < getal; i++) {
+		// Loop voor spaties. Begin: veel spaties, eind: geen spaties
+		for (int j = 2 * getal - 2 * i - 2; j > 0; j--) {
+			cout << " ";
+		}
+		// Loop voor getallen: met spatie ertussen
+		for (int j = 1; j <= 2*i + 1; j++) {
+			cout << j << " ";
+		}
+		// Volgende lijn
+		cout << endl;
+	}
 
 	return 0;
 }
